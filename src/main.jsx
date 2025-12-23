82aa76259057e23872f1b02c54824386e99435cb
김선영 (KIM SUNYOUNG) 과장 (Application SW팀) <sykim3@seegene.com>
2025-12-23 15:38:23 +0900

    feat: 설정 팝업 초기 버전 - 다중 선택 데이터 불러오기 방식 구현
    - 데이터 불러오기 방식 다중 선택 기능 추가
      - Manual, Preset, LIMS(.plrn), CSV, 바코드 선택 가능
      - LIMS와 CSV 상호 배타적 선택
      - 바코드 단독 선택 불가 (다른 방식과 함께 사용)
    - HL7 Abbreviation Setting 구현
      - Assay 테이블과 Target 테이블 분리
      - Assay Code, Target Code 수정 가능
    - 툴팁을 통한 규칙 안내
    - 반응형 UI 및 선택 상태 표시

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SettingsPopup from './SettingsPopup_v1_backup.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SettingsPopup />
+  </React.StrictMode>,
+)
