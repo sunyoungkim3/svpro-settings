2f1e8b11562ffb9d9dd3eafb7cfc06b7eccade05
김선영 (KIM SUNYOUNG) 과장 (Application SW팀) <sykim3@seegene.com>
2025-12-23 15:58:30 +0900

    fix: main.jsx import 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SettingsPopup from './SettingsPopup_v1_backup.jsx'
+import SettingsPopup from './SettingsPopup_v1.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
